c8422e405a0c5fc1416a555e25914c960ae09c28
Brandon Matthew <matthewrc95@gmail.com>
2022-07-18 18:57:31 -0700

    Added researchers views and images

--- app/controllers/admin/researchers_controller.rb
+++ app/controllers/admin/researchers_controller.rb
@@ -6,5 +6,31 @@ module Admin
     def index
       @researchers = Researcher.all
     end
+
+    def new
+      @researcher = Researcher.new
+    end
+
+    def show
+      @researcher = Researcher.find(params[:id])
+    end
+
+    def create
+      @researcher = Researcher.new(researcher_params)
+      if @researcher.save
+        redirect_to  root_path(@researcher.id), notice: 'El curso ha sido creado'
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+    
+    private
+
+    def researcher_params
+      params.fetch(:researcher).permit(
+        :academic_description, :main_language, :main_language_level, 
+        :contact_email, :user_id, :image
+      )    
+    end
   end
 end
