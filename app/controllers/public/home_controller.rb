bdf8728c8d6fe65eb3e78307ed3cbee03b1d6ec1
Brandon Matthew <matthewrc95@gmail.com>
2023-03-11 18:32:39 -0800

    Actualice la graficas quitando los nil

--- app/controllers/public/home_controller.rb
+++ app/controllers/public/home_controller.rb
@@ -22,15 +22,15 @@ module Public
     def graphs
       years_a = BibliographicFile.where.not(year: [nil,"0"]).order('year ASC').group(:year).count
       years_b = BibliographicFile.where(year: [nil,"0"]).count
-      @years = { nil => years_b }.merge(years_a)
+      @years = { "S/E" => years_b }.merge(years_a)
 
       language_a = BibliographicFile.where.not(language_y: [nil,"n",""]).group(:language_y).count
       language_b = BibliographicFile.where(language_y: [nil,"n", ""]).count
-      @language_y = { nil => language_b }.merge(language_a)
+      @language_y = { "S/E" => language_b }.merge(language_a)
 
       n1_a = BibliographicFile.where.not(n1: [nil,"n", ""]).group(:n1).count
       n1_b = BibliographicFile.where(n1: [nil,"n", ""]).count
-      @n1 = { nil => n1_b }.merge(n1_a)
+      @n1 = { "S/E" => n1_b }.merge(n1_a)
     
 
     end
