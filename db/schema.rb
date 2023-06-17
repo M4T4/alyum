8d74b7749a123577bb400844f9b555c55b9d3c5c
Brandon Matthew <matthewrc95@gmail.com>
2023-06-16 19:25:14 -0700

    Updated icons:

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_16_215107) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_17_020253) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -114,6 +114,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_16_215107) do
     t.datetime "updated_at", null: false
     t.string "b_data"
     t.boolean "ms"
+    t.boolean "pdf"
+    t.boolean "has_access"
   end
 
   create_table "carousels", force: :cascade do |t|
