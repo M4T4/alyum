d30e551f9d75195096c355e8ea1324c06ec8ebcd
Brandon Matthew <matthewrc95@gmail.com>
2023-09-29 11:05:29 +0930

    Updated ransack attributed

--- app/models/bibliographic_file.rb
+++ app/models/bibliographic_file.rb
@@ -5,4 +5,16 @@ class BibliographicFile < ApplicationRecord
 	has_paper_trail
 
   has_many :secondary_authors
+
+	def self.ransackable_attributes(auth_object = nil)
+    ["acces_link", "b_data", "base", "city_country", "country", "created_at", "document_language", 
+			"document_type", "doi", "editor_record", "free_lock", "gender", "has_access", "id", "isbn", 
+			"issn", "journal_bookname", "language_y", "language_y2", "ms", "n1", "n2", "page_number", 
+			"pdf", "publisher", "researcher_first_name", "researcher_last_name", "title", "updated_at", 
+			"volume", "volume_number", "year"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["secondary_authors", "versions"]
+  end
 end
