338cf0a8dedeb5e1c4457d3c398c5535543e5eef
Brandon Matthew <matthewrc95@gmail.com>
2023-10-03 13:24:28 +1030

    Added index files

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,7 @@ Rails.application.routes.draw do
       post "add_researcher", to: "bibliographic_files#add_researcher"
       post "remove_researcher", to: "bibliographic_files#remove_researcher"
     end
+    resources :lexico_files
     resources :researchers do
       get 'user_researcher', on: :new
     end
