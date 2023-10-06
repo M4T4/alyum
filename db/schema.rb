eb0112386637589fdbcb81beb0154de901919c4d
Brandon Matthew <matthewrc95@gmail.com>
2023-10-06 10:50:55 +1030

    Created alphabets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_02_235635) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_06_001838) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -82,6 +82,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_02_235635) do
     t.datetime "started_at"
     t.index ["user_id"], name: "index_ahoy_visits_on_user_id"
     t.index ["visit_token"], name: "index_ahoy_visits_on_visit_token", unique: true
+  end
+
+  create_table "alphabets", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "bibliographic_files", force: :cascade do |t|
