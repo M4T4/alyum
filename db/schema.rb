393de0caafa3351b89056dffd11bc938466d666e
Brandon Matthew <matthewrc95@gmail.com>
2023-10-06 11:59:19 +1030

    Added alphabets with related MVC elements

--- db/schema.rb
+++ db/schema.rb
@@ -85,7 +85,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_06_001838) do
   end
 
   create_table "alphabets", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
