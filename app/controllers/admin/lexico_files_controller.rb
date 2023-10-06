cbb84b5c0c2e298adcb9ad3b537d92fdf0a40e8e
Brandon Matthew <matthewrc95@gmail.com>
2023-10-06 14:21:35 +1030

    Added lexico file create and forms with alphabet

--- app/controllers/admin/lexico_files_controller.rb
+++ app/controllers/admin/lexico_files_controller.rb
@@ -2,6 +2,7 @@
 
 module Admin
   class LexicoFilesController < BaseController
+
     def index
       @q = LexicoFile.ransack(params[:q])
       @pagy, @files = pagy(@q.result(distinct: true).order("id ASC"))
@@ -10,7 +11,25 @@ module Admin
 
     def new
       @lexico_file = LexicoFile.new
+      @alphabets = Alphabet.all
     end
 
+    def create
+      binding.break
+			@lexico_file = LexicoFile.new(lexico_params)
+			if @lexico_file.save
+				redirect_to admin_lexico_files_path, notice: 'Se agrego un nuevo archivo léxico'
+			else
+				render :new, status: :unprocessable_entity
+			end
+		end
+
+    private
+
+    def lexico_params
+      params.fetch(:lexico_file).permit(:id, :language, :word, :spanish_word, :english_word, 
+        :author, :year_of_publication, :page, :provider, :translated, :audio, 
+        :bibliographic_file_id, :alphabet_id)
+    end
   end
 end
